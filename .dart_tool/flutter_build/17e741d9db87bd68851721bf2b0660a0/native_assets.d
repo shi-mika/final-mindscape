e3ebf4b125ffb94ce322aa66541fb6e0c16091e1
shi.mika <gellecania.emika16@gmail.com>
2023-12-30 18:04:22 +0800

    1st commit w dependencies

---
+++ .dart_tool/flutter_build/17e741d9db87bd68851721bf2b0660a0/native_assets.d
@@ -0,0 +1,1 @@
+ D:\\Users\\emika\\AndroidStudioProjects\\final-mindscape\\.dart_tool\\flutter_build\\17e741d9db87bd68851721bf2b0660a0\\native_assets.yaml: 
---
+++ .dart_tool/flutter_build/29cc006c021d83eff5b33865a592b6f6/native_assets.d
@@ -0,0 +1,1 @@
+ D:\\Users\\emika\\AndroidStudioProjects\\final-mindscape\\.dart_tool\\flutter_build\\29cc006c021d83eff5b33865a592b6f6\\native_assets.yaml: 
